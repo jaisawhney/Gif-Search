a2d0f56c581590cd7ddcfd66e8c32ff3732d2e08
Jai Sawhney <jai.sawhney1@gmail.com>
2022-03-22 15:59:18 -0700

    Add search form

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.engine('handlebars', engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    res.render('home', {gifUrl})
-});
+    console.log(req.query)
+    res.render('home')
+})
 
 app.listen(3000, () => {
     console.log('Gif Search listening on port localhost:3000!');
