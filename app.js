fa9610b135cae82e46573db400973dec5e365777
Jai Sawhney <jai.sawhney1@gmail.com>
2022-03-22 16:11:53 -0700

    Gif Search

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
 const {engine} = require('express-handlebars');
@@ -8,8 +9,13 @@ app.engine('handlebars', engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    console.log(req.query)
-    res.render('home')
+    const term = req?.query?.term
+    fetch(`https://g.tenor.com/v1/search?q=${term || ""}&key=${process.env.API_KEY}&limit=10`)
+        .then(res => res.json())
+        .then(data => {
+            const gifs = data.results;
+            res.render('home', {gifs});
+        });
 })
 
 app.listen(3000, () => {
